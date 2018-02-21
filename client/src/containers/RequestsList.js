eece3534524b3fe39ca5e6f0bf3c8c4cca13f4f1
Bananaha <claire.poyo@gmail.com>
2018-02-22 00:55:01 +0100

    WIP ==> DEBUG REFRESH

--- client/src/containers/RequestsList.js
+++ client/src/containers/RequestsList.js
@@ -13,11 +13,12 @@ class RequestsList extends Component {
       alert: ""
     };
   }
-
+  // TODO ==> gérer le probleme du reload de page
   getFriendRequests = () => {
     get("/friendRequest")
       .then(({ requests }) => {
         console.log(requests);
+
         if (requests.length > 0) {
           this.setState({
             loading: false,
@@ -42,20 +43,31 @@ class RequestsList extends Component {
 
   answerRequest = (requestId, status, autor) => {
     console.log(requestId, status);
+    this.setState({
+      loading: true
+    });
+
     post(`/friendRequest/${status}`, { requestId: requestId })
       .then(() => {
-        this.getFriendRequests();
+        this.setState({
+          loading: false
+        });
+        console.log("reload");
+        return this.getFriendRequests();
         const confirmationMessage =
           status === "accept"
             ? `Vous êtes maintenant ami avec ${autor}`
             : `Vous avez rejeté l'invitation de ${autor}`;
+
         this.setState({
           alert: confirmationMessage
         });
+
         setTimeout(() => {
           this.setState({ alert: "" });
         }, 5000);
       })
+
       .catch(error => {
         this.setState({
           message: error.alert
@@ -72,36 +84,47 @@ class RequestsList extends Component {
           <div>
             {user ? (
               <div>
-                {this.state.requests.map(request => {
-                  if (user.pseudo === request.authorPseudo) {
-                    return (
-                      <div key={request._id}>
-                        <p>{request.recipientPseudo}</p>
-                        <span>En attente de confirmation</span>
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div key={request._id}>
-                        <p>{request.authorPseudo}</p>
-                        <button
-                          onClick={(requestId, status, autor) =>
-                            this.answerRequest(request._id, "accept")
-                          }
-                        >
-                          Accepter
-                        </button>
-                        <button
-                          onClick={(requestId, status) =>
-                            this.answerRequest(request._id, "ignore")
-                          }
-                        >
-                          Ignorer
-                        </button>
-                      </div>
-                    );
-                  }
-                })}
+                {this.state.requests.length > 0 ? (
+                  <div>
+                    {this.state.requests.map(request => {
+                      if (
+                        user.pseudo === request.author.pseudo ||
+                        user._id === request.author
+                      ) {
+                        return (
+                          <div key={request._id}>
+                            <p>
+                              {request.recipient.pseudo || request.recipient}
+                            </p>
+                            <span>En attente de confirmation</span>
+                          </div>
+                        );
+                      } else {
+                        return (
+                          <div key={request._id}>
+                            <p>{request.author.pseudo || request.author}</p>
+                            <button
+                              onClick={(requestId, status, autor) =>
+                                this.answerRequest(request._id, "accept")
+                              }
+                            >
+                              Accepter
+                            </button>
+                            <button
+                              onClick={(requestId, status) =>
+                                this.answerRequest(request._id, "ignore")
+                              }
+                            >
+                              Ignorer
+                            </button>
+                          </div>
+                        );
+                      }
+                    })}
+                  </div>
+                ) : (
+                  <p>{this.state.message}</p>
+                )}
               </div>
             ) : (
               "Loading"
